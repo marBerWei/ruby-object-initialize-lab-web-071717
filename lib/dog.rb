e25f6e3365e32a39211b4c54a70955a634b9c947
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-07-19 13:43:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+	
+	def initialize(name,k="Mutt")
+		@name = name
+		@breed = k
+	end 
+
+	def name
+		@name
+	end 
+
+end
+
+
+# class Animal
+  
+#   def species 
+#     @my_species = "cat"
+#   end
+
+#   def species=(species)
+#   	@my_species
+#   end
+  
+#   def say_species
+#     puts "Hi! I'm a #{species}"
+#   end
